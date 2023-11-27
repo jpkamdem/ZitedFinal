10254a8a1e18dd3dcee9aab5a3ab050db58ef392
jpkamdem <115159200+jpkamdem@users.noreply.github.com>
2023-11-27 19:40:33 +0100

    Modification taille des logos applications, ajout widget menu burger

--- src/views/Navbar/AppContainer.vue
+++ src/views/Navbar/AppContainer.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app-container">
-    <div id="burger-menu"></div>
+    <div class="burger">
+      <label class="burger" for="burger">
+        <input type="checkbox" id="burger" />
+        <span></span>
+        <span></span>
+        <span></span>
+      </label>
+    </div>
     <div id="icons-container">
       <a :href="spotify" target="_blank"
         ><img src="./../../assets/spotify.png" alt="ArtistLink" class="icon"
@@ -35,10 +42,67 @@ div#app-container {
   height: calc(100% - 9% - 2em);
   border-radius: 20px;
 }
-div#burger-menu {
+div.burger {
   height: 10%;
-  background: red;
+  position: relative;
+  width: 40px;
+  height: 30px;
+  background: transparent;
+  cursor: pointer;
+  display: block;
 }
+
+.burger input {
+  display: none;
+}
+
+.burger span {
+  display: block;
+  position: absolute;
+  height: 4px;
+  width: 100%;
+  background: white;
+  border-radius: 9px;
+  opacity: 1;
+  left: 0;
+  transform: rotate(0deg);
+  transition: 0.25s ease-in-out;
+}
+
+.burger span:nth-of-type(1) {
+  top: 0px;
+  transform-origin: left center;
+}
+
+.burger span:nth-of-type(2) {
+  top: 50%;
+  transform: translateY(-50%);
+  transform-origin: left center;
+}
+
+.burger span:nth-of-type(3) {
+  top: 100%;
+  transform-origin: left center;
+  transform: translateY(-100%);
+}
+
+.burger input:checked ~ span:nth-of-type(1) {
+  transform: rotate(45deg);
+  top: 0px;
+  left: 5px;
+}
+
+.burger input:checked ~ span:nth-of-type(2) {
+  width: 0%;
+  opacity: 0;
+}
+
+.burger input:checked ~ span:nth-of-type(3) {
+  transform: rotate(-45deg);
+  top: 28px;
+  left: 5px;
+}
+
 div#icons-container {
   height: 90%;
   display: flex;
@@ -46,10 +110,7 @@ div#icons-container {
   justify-content: end;
   align-items: center;
 }
-div#icons-container img {
-  height: 50%;
-}
 div#icons-container a img {
-  height: 4em;
+  height: 3em;
 }
 </style>
